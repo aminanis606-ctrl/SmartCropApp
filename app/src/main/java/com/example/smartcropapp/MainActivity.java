408b482ae3648f40b892cc890d1c4fa7102d1302
aminanis606-ctrl <aminanis606@users.noreply.github.com>
2026-08-30 10:45:57 +0700

    Fix missing InputStream import

--- app/src/main/java/com/example/smartcropapp/MainActivity.java
+++ app/src/main/java/com/example/smartcropapp/MainActivity.java
@@ -24,6 +24,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileInputStream;
 import java.io.OutputStream;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
