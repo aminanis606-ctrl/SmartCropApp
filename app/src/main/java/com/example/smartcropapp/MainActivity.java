eab94962b4fc3a0e9d776a7d5cf0dd6753e8af16
aminanis606-ctrl <aminanis606@users.noreply.github.com>
2026-08-30 12:04:13 +0700

    Separate export paths: JSON to Downloads, Video to Movies

--- app/src/main/java/com/example/smartcropapp/MainActivity.java
+++ app/src/main/java/com/example/smartcropapp/MainActivity.java
@@ -140,7 +140,7 @@ public class MainActivity extends AppCompatActivity {
         ContentValues values = new ContentValues();
         values.put(MediaStore.Video.Media.DISPLAY_NAME, displayName);
         values.put(MediaStore.Video.Media.MIME_TYPE, "video/mp4");
-        values.put(MediaStore.Video.Media.RELATIVE_PATH, Environment.DIRECTORY_DOWNLOADS + "/SmartReframe");
+        values.put(MediaStore.Video.Media.RELATIVE_PATH, Environment.DIRECTORY_MOVIES + "/SmartReframe");
         values.put(MediaStore.Video.Media.IS_PENDING, 1);
         
         Uri uri = getContentResolver().insert(MediaStore.Video.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY), values);
