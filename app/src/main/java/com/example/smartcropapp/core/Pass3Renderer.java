8e55abeb5dbb72596627e1c170f9bba6ece4b0ec
aminanis606-ctrl <aminanis606@users.noreply.github.com>
2026-09-01 18:17:44 +0700

    test: isolate SurfaceTexture transform in split render

--- app/src/main/java/com/example/smartcropapp/core/Pass3Renderer.java
+++ app/src/main/java/com/example/smartcropapp/core/Pass3Renderer.java
@@ -9,6 +9,7 @@ import android.media.MediaFormat;
 import android.media.MediaMuxer;
 import android.net.Uri;
 import android.opengl.GLES20;
+import android.opengl.Matrix;
 import android.util.Log;
 import android.view.Surface;
 
@@ -280,11 +281,13 @@ public class Pass3Renderer {
 
                         // Draw Top Panel (Speaker A)
                         GLES20.glViewport(0, panelH, OUTPUT_WIDTH, panelH);
-                        shader.draw(glContext.getDecoderTextureId(), stMatrix, topX, topY, cropWidthNorm, cropHeightNorm);
+                        float[] splitST = new float[16];
+                        Matrix.setIdentityM(splitST, 0);
+                        shader.draw(glContext.getDecoderTextureId(), splitST, topX, topY, cropWidthNorm, cropHeightNorm);
 
                         // Draw Bottom Panel (Speaker B)
                         GLES20.glViewport(0, 0, OUTPUT_WIDTH, panelH);
-                        shader.draw(glContext.getDecoderTextureId(), stMatrix, botX, botY, cropWidthNorm, cropHeightNorm);
+                        shader.draw(glContext.getDecoderTextureId(), splitST, botX, botY, cropWidthNorm, cropHeightNorm);
                     } else {
                         // Draw Single Full Screen (Center Crop)
                         GLES20.glViewport(0, 0, OUTPUT_WIDTH, OUTPUT_HEIGHT);
